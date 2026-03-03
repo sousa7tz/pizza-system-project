6d9cd917fc4fee9dd156cddf7e58f7f8897813e0
Emanuel G. Sousa <emanuelgoncalves434@gmail.com>
2026-03-02 22:15:47 -0300

    feat: schemas - done

--- backend/src/schemas.py
+++ backend/src/schemas.py
@@ -4,6 +4,44 @@ from pydantic import BaseModel
 # caso haja inconsistencia, ja da erro antes mesmo de passar pro CRUD
 # se nao houverem erros, o CRUD já faz o trabalho de traduzir e adicionar no banco.
 
+class OrderCreate(BaseModel):
+    employee_id: int
+    customer_id: int
+    delivery_driver_id: int
+    is_delivery: bool
+    payment_type: str
+    address: str
+    status: str
+
+class Order(OrderCreate):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+class OrderItemCreate(BaseModel):
+    order_id: int
+    product_id: int
+    quantity: int
+    unit_price: float
+
+class OrderItem(OrderItemCreate):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+class CustomerCreate(BaseModel):
+    name: str
+    address: str
+    phone_number: str
+
+class Customer(CustomerCreate):
+    id: int
+
+    class Config:
+        from_attributes = True
+
 # table products schema
 class ProductCreate(BaseModel):
     name: str
@@ -18,3 +56,27 @@ class Product(ProductCreate):
     class Config:
         from_attributes = True # pydantic ler os dados do sqlalchemy
 
+class EmployeeCreate(BaseModel):
+    address: str
+    phone_number: str
+    email: str
+    role: str
+    name: str
+
+class Employee(EmployeeCreate):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+class DeliveryDriverCreate(BaseModel):
+    name: str
+    phone_number: str
+    vehicle: str
+    vehicle_plate: str
+
+class DeliveryDriver(DeliveryDriverCreate):
+    id: int
+
+    class Config:
+        from_attributes = True
